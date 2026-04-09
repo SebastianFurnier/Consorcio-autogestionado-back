bb00d42ac0e02eb4b090a6307e2b6747d512cdcc
Sebastian Furnier <sfurnier@hotmail.com>
2026-04-09 19:52:46 -0300

    First commit

---
+++ src/test/java/com/tpgdb/Consorcio/ConsorcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpgdb.Consorcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
