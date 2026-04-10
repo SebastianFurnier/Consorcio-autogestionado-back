74530a81568dd54e4f9511209fbdad502fb44a2c
Sebastian Furnier <sfurnier@hotmail.com>
2026-04-09 23:08:29 -0300

    Incluyo configuracion de Docker

--- src/main/java/com/tpgdb/Consorcio/Controller/PartnerController.java
+++ src/main/java/com/tpgdb/Consorcio/Controller/PartnerController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/partner")
+@CrossOrigin("*")
 public class PartnerController {
     final private PartnerService service;
 
